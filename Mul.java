5dbd2b78547e789ad03124a00e8864e4ba73c188
Hasan shareef mohd <107669781+HasanShareefMohd@users.noreply.github.com>
2022-06-22 13:47:13 +0530

    Add files via upload

---
+++ Mul.java
@@ -0,0 +1,8 @@
+class Mul{
+public static void main(String[] args){
+int a=5;
+int b=6;
+int sum=a*b;
+System.out.println(sum);
+}
+}
---
+++ Practice1.java
@@ -0,0 +1,10 @@
+class Practice1{
+   public static void main (String[]args){
+    System.out.println("Hasan shareef mohd.");
+System.out.println("IT Programming.");
+    System.out.println("computer science and engineering.");
+    
+
+
+}
+}
